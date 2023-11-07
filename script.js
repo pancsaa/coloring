ab92a3bc69d74dd398f256b82fd966d4c19b7fc4
pancsaa <pampukp@gmail.com>
2023-11-07 10:03:32 +0100

    hahaha

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const content=document.querySelector(".content")
-for(let i=0;i<64;i++){
-    //template literal ALtGr+7 - ${változo}
-    content.innerHTML=`<div class="box" id="${i+1}"></div>`;
+const content = document.querySelector(".content");
+for(let i = 0; i < 64; i++){
+    //Template literal Altgr+7 - ${változó}
+    content.innerHTML += `<div class="box" id="${i+1}"></div>`;
 }
-//NodeList-ént megkapjuk a .box-okat:
-const boxes=document.querySelectorAll(".box");
-boxes.forEach(element=>{
-    element.addEventListener("click", (event) =>{
-        event.target.style.backgroundColor="#ff0000";
+//NodeList-ként megkapjuk a .box-okat:
+const boxes = document.querySelectorAll(".box");
+//Végig megyünk az egyes div-eken, és rájuk akasztunk egy-egy eseményfigyelőt:
+boxes.forEach(element => {
+    //Az eseményfigyelő a click eseményre indul be:
+    element.addEventListener("click", (event) => {
+        event.target.style.backgroundColor = "#ff0000";
     })
 })
