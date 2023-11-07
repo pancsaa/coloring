a1fda73b3ee10991c8b6cc232f014d5b7f77741f
pancsaa <pampukp@gmail.com>
2023-11-07 11:29:52 +0100

    interaktiv

--- script.js
+++ script.js
@@ -6,9 +6,34 @@ for(let i = 0; i < 64; i++){
 //NodeList-ként megkapjuk a .box-okat:
 const boxes = document.querySelectorAll(".box");
 //Végig megyünk az egyes div-eken, és rájuk akasztunk egy-egy eseményfigyelőt:
+
 boxes.forEach(element => {
     //Az eseményfigyelő a click eseményre indul be:
+    
     element.addEventListener("click", (event) => {
-        event.target.style.backgroundColor = "#ff0000";
+        //console.log(event.target);
+        let currentElement=event.target;
+        if(currentElement.style.backgroundColor===""){
+            currentElement.style.backgroundColor="#ff0000";
+            currentElement.textContent = currentElement.id;
+        }
+        else{
+            currentElement.style.backgroundColor="";
+            currentElement.textContent ="";//null
+        }
+
+        //currentElement.style.backgroundColor = "#ff0000";
+        currentElement.style.textAlign = "center";
+        //currentElement.textContent = currentElement.id;
     })
-})
+
+});
+
+/*(hasznalhatatlan)
+boxes.forEach(szinez(element));
+function szinez(element){
+    element.addEventListener("click", (event)=>{
+        event.target.style.backgroundColor="#ff0000";
+    })
+}
+*/
